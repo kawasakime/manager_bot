6a920df604896d977d9716b107fb681379c98d6c
wyylame <wyylame@yandex.ru>
2023-04-11 22:48:22 +0300

    new features

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 const cron = require("node-cron");
 
 require("dotenv").config();
 
 const workers = require("./workers");
 
-console.log(workers)
+console.log(workers);
 const bot = new Telegraf(process.env.TELEGRAM_API_TOKEN);
-const chat_id = -872858162
+const chat_id = -872858162;
 
-//back 10-14
-//front 10-16, 10-14
-//test 15-19
+const reply_chat_id = -875342255;
 
 let checkMessages = false;
 let wellDone = [];
 
-// const usersHours = workers.map(item => ({user: item.user, hours: 0}))
+/////////////////
+
+const NOT_WORK_KEYBOARD = [
+  [{ text: "Этот час не работаю", callback_data: "notWork" }],
+  [],
+  [],
+];
+
+////////////////
 
 function checkTime(time) {
   const hour = new Date().getHours();
@@ -27,7 +33,7 @@ function checkTime(time) {
 function checkLastTIme(time) {
   const hour = new Date().getHours();
 
-  return time.end === hour
+  return time.end === hour;
 }
 
 cron.schedule(
@@ -39,34 +45,37 @@ cron.schedule(
       worker.all ? checkTime(worker.work) : checkTime(worker.days[day])
     );
 
-    const lastTimeWorkers = workers.filter(worker => worker.all ? checkLastTIme(worker.work) : checkLastTIme(worker.days[day]))
+    const lastTimeWorkers = workers.filter((worker) =>
+      worker.all ? checkLastTIme(worker.work) : checkLastTIme(worker.days[day])
+    );
 
-    console.log('CURRENT WORKERS', currentWorkers)
+    console.log("CURRENT WORKERS", currentWorkers);
 
     if (currentWorkers.length > 0) {
-      const markWorkers = currentWorkers.map((worker) => `@${worker.user}`).join(", ");
+      const markWorkers = currentWorkers
+        .map((worker) => `@${worker.user}`)
+        .join(", ");
       const message = `${markWorkers} напишите в чат своё текущее задание. \nЕсли задания нет, то возьмите его у Максима, далее уведомите о нём в текущий чат.`;
-  
-      bot.telegram.sendMessage(chat_id, message);
+
+      bot.telegram.sendMessage(chat_id, message, {
+        reply_markup: {
+          keyboard: NOT_WORK_KEYBOARD,
+          one_time_keyboard: true,
+          resize_keyboard: true,
+        },
+      });
 
       checkMessages = true;
     }
 
     if (lastTimeWorkers.length > 0) {
-      const markWorkers = lastTimeWorkers.map((worker) => `@${worker.user}`).join(", ");
+      const markWorkers = lastTimeWorkers
+        .map((worker) => `@${worker.user}`)
+        .join(", ");
       const message = `${markWorkers} напишите на какой задаче вы закончили рабочий день и выполнили ли вы её до конца?`;
 
       bot.telegram.sendMessage(chat_id, message);
     }
-
-    // if (new Date().getHours() === 19) {
-      // const hoursList = usersHours.filter(item => item.hours > 0).map(item => `${item.user} - ${item.hours} часов`)
-      // const today = `${new Date().getDate()}.${new Date().getMonth()}`
-      // const message = `Количество часов за ${today}:\n` + hoursList.join('\n')
-
-      // bot.telegram.sendMessage(chat_id, message)
-    // }
-
   },
   {
     scheduled: true,
@@ -77,23 +86,26 @@ cron.schedule(
 cron.schedule(
   "15 9-19 * * 1-5",
   () => {
-
     const day = new Date().getDay();
 
     const currentWorkers = workers.filter((worker) =>
       worker.all ? checkTime(worker.work) : checkTime(worker.days[day])
     );
 
-    const badUsers = currentWorkers.filter((worker) => !wellDone.includes(worker.user))
+    const badUsers = currentWorkers.filter(
+      (worker) => !wellDone.includes(worker.user)
+    );
 
     if (badUsers.length > 0) {
-      const badUsersStr = badUsers.map(worker => `@${worker.user}`).join(', ')
+      const badUsersStr = badUsers
+        .map((worker) => `@${worker.user}`)
+        .join(", ");
       const message = `${badUsersStr} - не написали ничего в чат.\n Отмечаю @kawasakime`;
-  
+
       bot.telegram.sendMessage(chat_id, message);
     }
 
-    wellDone = []
+    wellDone = [];
     checkMessages = false;
   },
   {
@@ -104,15 +116,29 @@ cron.schedule(
 
 bot.start((ctx) => ctx.reply("Привет."));
 bot.on("message", (ctx) => {
-  if (checkMessages) {
-    const user = ctx.message.from.username;
+  console.log(ctx);
+  // console.log(ctx.message.chat);
+  // console.log(ctx);
+  console.log(ctx.update.message.chat);
 
-    if (workers.map((worker) => worker.user).includes(user)) {
+  const user = ctx.message.from.username;
+  const isWorker = workers.map((worker) => worker.user).includes(user);
+
+  if (ctx.message.text === "Этот час не работаю" && isWorker) {
+    wellDone.push(user);
+    return;
+  }
+
+  if (checkMessages && ctx.message.chat.id === chat_id) {
+    if (isWorker) {
+      if (!wellDone.includes(user)) {
+        ctx.forwardMessage(reply_chat_id);
+      }
       wellDone.push(user);
-      // usersHours[user].hours += 1
     }
   }
 });
+
 bot.launch();
 
 // Enable graceful stop
